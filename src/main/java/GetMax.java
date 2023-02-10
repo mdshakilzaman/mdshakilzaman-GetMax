1b0428fb2d7a97eb4adcbc751c21a99dd2c7d8fb
Md Shakil Zaman <mdshakilzaman2020@gmail.com>
2023-02-10 23:36:43 +0000

    first commit

--- src/main/java/GetMax.java
+++ src/main/java/GetMax.java
@@ -7,6 +7,12 @@ public class GetMax {
      * @return the largest value in arr.
      */
     public int max(int[] arr){
-        return -1;
+        int max_val = arr[0];
+        for (int i = 1; i < arr.length; i++){
+            if (arr[i] > max_val){
+                max_val = arr[i];
+            }
+        }
+        return max_val;
     }
 }
